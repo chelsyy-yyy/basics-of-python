40e93820139912b9a50d5fdd27827576d325d303
chelsyy-yyy <chelsyabb@gmail.com>
2025-12-10 17:34:47 +0000

    Add files via upload

---
+++ basics of python.py
@@ -0,0 +1,28 @@
+#question 1
+playlist =["God's Plan","Work","All my life", "Girls Want Girls","Trust Issues"]
+print(playlist)
+
+#question 2
+top_artist = ("Drake","Kendrick Lamar","Nathaniel Bassey","Rihanna")
+print(top_artist)
+
+#question 3
+my_music = [playlist,top_artist]
+print(my_music)
+
+#question 4
+print(len(my_music))
+
+#question 5
+print(playlist[2])
+
+#question 6
+playlist[0] = "Perfect"
+print(playlist)
+
+#question 7
+playlist.append("Blinding Lights")
+print(playlist)
+
+#question 8
+print(playlist)
